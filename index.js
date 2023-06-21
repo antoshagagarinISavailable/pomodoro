f806b9c23adafc6b8bab70bbf315ee3de8984d58
antoshagagarin <antoshagagarin1@gmail.com>
2023-06-22 01:12:49 +0300

    some changes

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+//создаем объекты которые содержат информацию о режиме таймера
+const pomodoro = {
+  minutes: JSON.parse(localStorage.getItem("pomodoro"))
+    ? JSON.parse(localStorage.getItem("pomodoro")).minutes
+    : 25,
+  seconds: 0,
+  set setMinutes(value) {
+    this.minutes = +value;
+  },
+  get time() {
+    return this.minutes * 60;
+  },
+};
+const quickBreak = {
+  minutes: JSON.parse(localStorage.getItem("quickBreak"))
+    ? JSON.parse(localStorage.getItem("quickBreak")).minutes
+    : 5,
+  seconds: 0,
+  set setMinutes(value) {
+    this.minutes = +value;
+  },
+  get time() {
+    return this.minutes * 60;
+  },
+};
+const longBreak = {
+  minutes: JSON.parse(localStorage.getItem("longBreak"))
+    ? JSON.parse(localStorage.getItem("longBreak")).minutes
+    : 15,
+  seconds: 0,
+  set setMinutes(value) {
+    this.minutes = +value;
+  },
+  get time() {
+    return this.minutes * 60;
+  },
+};
+
 // все const по settings
 
 //html модалки setting
@@ -11,12 +49,23 @@ const setLongBreak = document.querySelector("#setLongBreak");
 //кнопки для settings
 const mainSettingsButton = document.querySelector("#mainSettingsButton");
 const settingsCloseButton = document.querySelector("#settingsCloseButton");
+const settingsSaveButton = document.querySelector("#settingsSaveButton");
 
 //слушатели по модалке настроек
 mainSettingsButton.addEventListener("click", () => {
   settingsModal.classList.toggle("none");
 });
 settingsCloseButton.addEventListener("click", () => {
+  settingsModal.classList.toggle("none");
+});
+settingsSaveButton.addEventListener("click", () => {
+  pomodoro.setMinutes = setPomodoro.value;
+  longBreak.setMinutes = setLongBreak.value;
+  quickBreak.setMinutes = setQuickBreak.value;
+  localStorage.setItem("pomodoro", JSON.stringify(pomodoro));
+  localStorage.setItem("quickBreak", JSON.stringify(quickBreak));
+  localStorage.setItem("longBreak", JSON.stringify(longBreak));
+
   settingsModal.classList.toggle("none");
 });
 
@@ -45,8 +94,10 @@ cancelNewTodo.addEventListener("click", () => {
 
 // блок таймера
 
-//цифры таймера
-const timerString = document.querySelector(".timer-string");
+//минуты таймера
+const timerMinutes = document.querySelector(".timer-string__minutes");
+//секунды таймера
+const timerSeconds = document.querySelector(".timer-string__seconds");
 
 //кнопки выбора режима
 const quickBreakChooseButton = document.querySelector(
@@ -55,13 +106,64 @@ const quickBreakChooseButton = document.querySelector(
 const longBreakChooseButton = document.querySelector("#longBreakChooseButton");
 const pomodoroChooseButton = document.querySelector("#pomodoroChooseButton");
 const changeModeButtons = document.querySelectorAll(".timer-mode-list button");
-console.log(changeModeButtons);
+
+//кнопка start
+const startButton = document.querySelector("#startButton");
+
+//глобальные переменные для таймера
+let interval;
+let timerIsActive = false;
+
+//функция запуска таймера
+const timerStartFunction = () => {
+  timerIsActive = !timerIsActive;
+  //рендер текста для кнопки start
+  startButton.textContent = timerIsActive ? "pause" : "start";
+  let time = +timerMinutes.textContent * 60 + +timerSeconds.textContent;
+  clearInterval(interval);
+  interval = setInterval(() => {
+    if (time > 0 && timerIsActive) {
+      time--;
+      let minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+      console.log(time);
+      timerMinutes.textContent = minutes >= 10 ? `${minutes}` : `0${minutes}`;
+      timerSeconds.textContent = seconds >= 10 ? `${seconds}` : `0${seconds}`;
+    }
+  }, 100);
+};
+
+//слушатель по нажатию кнопки start
+startButton.addEventListener("click", timerStartFunction);
+
 //слушатели по нажатию кнопок выбора режима
-changeModeButtons.forEach((el) => {
-  el.addEventListener("click", () => {
-    changeModeButtons.forEach((el) => {
-      el.classList.remove("timer-mode-list_active");
-    });
-    el.classList.add("timer-mode-list_active");
-  });
+
+pomodoroChooseButton.addEventListener("click", () => {
+  longBreakChooseButton.classList.remove("timer-mode-list_active");
+  quickBreakChooseButton.classList.remove("timer-mode-list_active");
+  pomodoroChooseButton.classList.add("timer-mode-list_active");
+
+  time = timerMinutes.textContent =
+    pomodoro.minutes >= 10 ? pomodoro.minutes : "0" + pomodoro.minutes;
 });
+quickBreakChooseButton.addEventListener("click", () => {
+  longBreakChooseButton.classList.remove("timer-mode-list_active");
+  pomodoroChooseButton.classList.remove("timer-mode-list_active");
+  quickBreakChooseButton.classList.add("timer-mode-list_active");
+
+  timerMinutes.textContent =
+    quickBreak.minutes >= 10 ? quickBreak.minutes : "0" + quickBreak.minutes;
+});
+longBreakChooseButton.addEventListener("click", () => {
+  quickBreakChooseButton.classList.remove("timer-mode-list_active");
+  pomodoroChooseButton.classList.remove("timer-mode-list_active");
+  longBreakChooseButton.classList.add("timer-mode-list_active");
+
+  timerMinutes.textContent =
+    longBreak.minutes >= 10 ? longBreak.minutes : "0" + longBreak.minutes;
+});
+
+//по дефолту при каждой перезагрузе ставим количество минут из режима pomodoro
+timerMinutes.textContent = JSON.parse(localStorage.getItem("pomodoro"))
+  ? JSON.parse(localStorage.getItem("pomodoro")).minutes
+  : 25;
